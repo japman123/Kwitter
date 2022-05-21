3227cecdd2ed8948b997a5570404a1f011d72674
japman123 <87320271+japman123@users.noreply.github.com>
2022-05-20 21:57:23 -0400

    Add files via upload

--- kwitter.js
+++ kwitter.js
@@ -1,8 +1,5 @@
-function addUser() {
-
-    user_name = document.getElementById("user_name").value;
-  
+function add_User() {
+    user_name = document.getElementById("inputlogin").value;
     localStorage.setItem("user_name", user_name);
-    
-      window.location = "kwitter_room.html";
-  }
+    window.location = "kwitter_room.html"
+}
